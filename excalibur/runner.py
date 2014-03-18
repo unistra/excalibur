a2c949eba24c2028ddf6d1aba70178f7ca451824
Morgan <morgan.bohn@unistra.fr>
2014-03-18 18:29:18 +0100

    add runner

---
+++ excalibur/runner.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+class to run all the process
+"""
+
+from excalibur.loader import ConfigurationLoader
+from excalibur.utils import check_all, run_plugins
+
+class PluginsRunner(object):
+
+    def __init__(self, acl_file, sources_file, ressources_file, plugins_module, query):
+        self.__acl_file = acl_file
+        self.__sources_file = sources_file
+        self.__ressources_file = ressources_file
+        self.__plugins_module = plugins_module
+        self.__query = query
+
+
+    def __call__(self):
+        acl = ConfigurationLoader(self.__acl_file).content
+        sources = ConfigurationLoader(self.__sources_file).content
+        ressources = ConfigurationLoader(self.__ressources_file).content
+
+        check_all(acl, sources, ressources, self.__query['request_method'], 
+                self.__query['source'], self.__query['ressource'], self.__query['method'], self.__query['remote_ip'], 
+                self.__query['arguments'], self.__query['signature'])
+
+        data, errors = run_plugins(sources[self.__query['source']]["plugins"], self.__query['ressource'], 
+            self.__query['method'], self.__query['arguments'], self.__query['source'], self.__plugins_module)
+
+        return data, errors
+
--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -4,7 +4,7 @@
 from unittest import TestCase, main
 from excalibur.utils import run_plugins
 from excalibur.loader import ConfigurationLoader
-
+from excalibur.runner import PluginsRunner
 
 class PluginsTest(TestCase):
 
@@ -42,6 +42,44 @@ class PluginsTest(TestCase):
         data, errors = run_plugins(self.sources[self.source]["plugins"], self.ressource, self.method, 
             self.arguments, self.source, self.plugin_module)
 
+        self.assertEqual(errors, self.errors_raw)
+        self.assertEqual(data, {})
+
+
+class RunnerTest(TestCase):
+
+    def setUp(self):
+
+        self.query = { 'source': "etab1",
+                  'remote_ip' : "127.0.0.1",
+                  'signature' : "c08b3ff9dff7c5f08a1abdfabfbd24279e82dd10",
+                  'arguments' : { "login": "testzombie1", },
+                  'ressource' : "actions",
+                  'method' : "action1",
+                  'request_method' : "GET"
+                }
+
+        self.acl_path = "./data/acl.yml"
+        self.sources_path = "./data/sources.yml"
+        self.ressources_path = "./data/ressources.yml"
+        self.plugin_module = "tests.plugins"
+
+        self.data_ok = {'Plugin1': 'p1ok1', 'Plugin2': 'p2ok1'}
+        self.errors_raw = {'Plugin1': {'method': 'action2', 'source': 'etab1', 'arguments': {'login': 'testzombie1'}, 'error': 'Exception', 'ressource': 'actions', 'parameters_index': 0}, 'Plugin2': {'method': 'action2', 'source': 'etab1', 'arguments': {'login': 'testzombie1'}, 'error': 'Exception', 'ressource': 'actions', 'parameters_index': 0}}
+
+
+
+    def test_runner(self):
+        plugins_runner = PluginsRunner(self.acl_path, self.sources_path, self.ressources_path, self.plugin_module, self.query)
+        data, errors = plugins_runner()
+        self.assertEqual(data, self.data_ok)
+        self.assertEqual(errors, {})
+
+
+    def test_runner_errors(self):
+        self.query['method'] = "action2"
+        plugins_runner = PluginsRunner(self.acl_path, self.sources_path, self.ressources_path, self.plugin_module, self.query)
+        data, errors = plugins_runner()
         self.assertEqual(errors, self.errors_raw)
         self.assertEqual(data, {})
 
