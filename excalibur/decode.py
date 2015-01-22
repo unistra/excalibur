a968c7b417433abfb8bd5084d9258a5f0077b716
geoffroybeck <geoffroy.beck@gmail.com>
2015-01-22 16:50:38 +0100

    base64url

--- excalibur/decode.py
+++ excalibur/decode.py
@@ -61,6 +61,13 @@ class DecodeArguments(object):
 
         return self.f(self.obj, *k, **kw)
 
+    def decode_base64url(self,value):
+        """
+        safeb64
+        """
+        decoded_value = base64.urlsafe_b64decode(value).decode("utf-8")
+        return decoded_value
+
     def decode_base64(self, value):
         """
         Implemente le decodage base64.
