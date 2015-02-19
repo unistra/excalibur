991d653f01217c96c4dff390a038efcfddb14626
tschlecht <thierry.schlecht@gmail.com>
2015-02-19 10:44:25 +0100

    Add a base64url encode method

--- excalibur/decode.py
+++ excalibur/decode.py
@@ -69,3 +69,11 @@ class DecodeArguments(object):
         # base64 decoding returns bytestrings
         decoded_value = base64.b64decode(value).decode("utf-8")
         return decoded_value
+
+    def decode_base64url(self, value):
+        """
+        safeb64
+        """
+        decoded_value = base64.urlsafe_b64decode(value.encode('utf-8'))\
+            .decode("utf-8")
+        return decoded_value
